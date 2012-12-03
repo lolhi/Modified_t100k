7820e709b7fabba27bb6d763fb022580c35eb99b
985hPaKicK <gpffpdh@naver.com>
2012-12-03 22:18:09 +0900

    t100k_mod: skytestserver needs two functions

--- hardware/libhardware_legacy/wifi/wifi.c
+++ hardware/libhardware_legacy/wifi/wifi.c
@@ -950,3 +950,16 @@ int wifi_change_fw_path(const char *fwpath)
     close(fd);
     return ret;
 }
+
+//cm10 t100k for skytestserver
+#ifdef T100K
+int wifi_load_mfg_driver()
+{
+    return 0;
+}
+
+int wifi_unload_mfg_driver()
+{
+    return 0;
+}
+#endif
